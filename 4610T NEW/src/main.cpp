65ac6f6939d3a46b893d6af9e333da9e7369483c
adityakannan1 <136399177+adityakannan1@users.noreply.github.com>
2024-03-05 22:02:28 -0500

    Update main.cpp

--- 4610T NEW/src/main.cpp
+++ 4610T NEW/src/main.cpp
@@ -20,78 +20,8 @@
 using namespace vex;
 competition Competition;
 
-
-Drive chassis(
-
-//Specify your drive setup below. There are seven options:
-//ZERO_TRACKER_NO_ODOM, ZERO_TRACKER_ODOM, TANK_ONE_ENCODER, TANK_ONE_ROTATION, TANK_TWO_ENCODER, TANK_TWO_ROTATION, HOLONOMIC_TWO_ENCODER, and HOLONOMIC_TWO_ROTATION
-//For example, if you are not using odometry, put ZERO_TRACKER_NO_ODOM below:
-ZERO_TRACKER_ODOM,
-
-//Add the names of your Drive motors into the motor groups below, separated by commas, i.e. motor_group(Motor1,Motor2,Motor3).
-//You will input whatever motor names you chose when you configured your robot using the sidebar configurer, they don't have to be "Motor1" and "Motor2".
-
-//Left Motors:
-motor_group(leftFrontMotor,leftBackMotor),
-
-//Right Motors:
-motor_group(rightFrontMotor,rightBackMotor),
-
-//Specify the PORT NUMBER of your inertial sensor, in PORT format (i.e. "PORT1", not simply "1"):
-PORT12,
-
-//Input your wheel diameter. (4" omnis are actually closer to 4.125"):
-4.125,
-
-//External ratio, must be in decimal, in the format of input teeth/output teeth.
-//If your motor has an 84-tooth gear and your wheel has a 60-tooth gear, this value will be 1.4.
-//If the motor drives the wheel directly, this value is 1:
-0.66,
-
-//Gyro scale, this is what your gyro reads when you spin the robot 360 degrees.
-//For most cases 360 will do fine here, but this scale factor can be very helpful when precision is necessary.
-360,
-
-/*---------------------------------------------------------------------------*/
-/*                                  PAUSE!                                   */
-/*                                                                           */
-/*  The rest of the drive constructor is for robots using POSITION TRACKING. */
-/*  If you are not using position tracking, leave the rest of the values as  */
-/*  they are.                                                                */
-/*---------------------------------------------------------------------------*/
-
-//If you are using ZERO_TRACKER_ODOM, you ONLY need to adjust the FORWARD TRACKER CENTER DISTANCE.
-
-//FOR HOLONOMIC DRIVES ONLY: Input your drive motors by position. This is only necessary for holonomic drives, otherwise this section can be left alone.
-//LF:      //RF:    
-PORT1,     -PORT2,
-
-//LB:      //RB: 
-PORT3,     -PORT4,
-
-//If you are using position tracking, this is the Forward Tracker port (the tracker which runs parallel to the direction of the chassis).
-//If this is a rotation sensor, enter it in "PORT1" format, inputting the port below.
-//If this is an encoder, enter the port as an integer. Triport A will be a "1", Triport B will be a "2", etc.
-3,
-
-//Input the Forward Tracker diameter (reverse it to make the direction switch):
-2.75,
-
-//Input Forward Tracker center distance (a positive distance corresponds to a tracker on the right side of the robot, negative is left.)
-//For a zero tracker tank drive with odom, put the positive distance from the center of the robot to the right side of the drive.
-//This distance is in inches:
-6,
-
-//Input the Sideways Tracker Port, following the same steps as the Forward Tracker Port:
-1,
-
-//Sideways tracker diameter (reverse to make the direction switch):
--2.75,
-
-//Sideways tracker center distance (positive distance is behind the center of the robot, negative is in front):
-5.5
-
-);
+//vex initialization
+Drive chassis(ZERO_TRACKER_ODOM,motor_group(leftFrontMotor,leftBackMotor),motor_group(rightFrontMotor,rightBackMotor),PORT12,4.125,0.66,360,PORT1,-PORT2,PORT3,-PORT4,3,2.75,6,1,-2.75,5.5);
 
 int current_auton_selection = 4;
 bool auto_started = false;
@@ -101,9 +31,9 @@ void pre_auton(void) {
   vexcodeInit();
   default_constants();
 
-  while(auto_started == false){            //Changing the names below will only change their names on the
-    Brain.Screen.clearScreen();            //brain screen for auton selection.
-    switch(current_auton_selection){       //Tap the brain screen to cycle through autons.
+  while(auto_started == false){          
+    Brain.Screen.clearScreen();            
+    switch(current_auton_selection){       
       case 1:
         Brain.Screen.printAt(50, 50, "Far Side Quals");
         break;
